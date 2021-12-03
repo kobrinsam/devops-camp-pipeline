7898ab90b0144a0a627c1c147587bcb137cf718a
kobrinsam <kobrinsam@gmail.com>
2021-12-03 19:34:36 +0000

    fixing mistakes

--- harbor_scanner.py
+++ harbor_scanner.py
@@ -1,10 +1,8 @@
-import requests, sys, json, getopt
+import requests, time, sys, json, getopt
 
 ## Arguments needed from user ##
 argList = sys.argv[1:]
-
-## Modified to add -h option ##
-options = 'c:i:p:r:h:'
+options = 'c:i:p:r:'
 
 arguments, values = getopt.getopt(argList, options)
 for currentArgument, currentValue in arguments:
@@ -17,28 +15,37 @@ for currentArgument, currentValue in arguments:
     elif currentArgument in ("-r"):
         registry = currentValue
 
-    ## Added argument for DB_HASH ##
-    elif currentArgument in ("-h"):
-        pipelineDbHash = currentValue
+## Grab sha256 digest from Harbor project repository ##
+urlArtifact = 'https://' + registry + '/api/v2.0/projects/' + projectName + '/repositories/' + imageName + '/artifacts/'
+digestResp = requests.get(urlArtifact, auth=(username, password))
+artifactReference = digestResp.json()[0]['digest']
 
-## Added exception handling when image doesn't exist in harbor ##
-try:
-    ## Grab sha256 digest from Harbor project repository ##
-    urlArtifact = 'https://' + registry + '/api/v2.0/projects/' + projectName + '/repositories/' + imageName + '/artifacts/'
-    digestResp = requests.get(urlArtifact, auth=(username, password))
-    artifactReference = digestResp.json()[0]['digest']
+## Initialize image scanner ##
+urlScanInit = urlArtifact + artifactReference + '/scan'
+scanInitResp = requests.post(urlScanInit, data={}, auth=(username, password))
+if scanInitResp.status_code != 202:
+    print('Failed to scan image')
+    print('Server response code:', scanInitResp.status_code)
+    sys.exit(-1)
 
-    ## Check harbor DB commit hash in image tag ##
-    urlArtifactHashTag = urlArtifact + artifactReference + '/tags'
-    hashTagResp = requests.get(urlArtifactHashTag, auth=(username, password))
-    hashTag = hashTagResp.json()[0]['name']
-    harborDbHash, harborPipelineHash = hashTag.split('-')
+## Checks scanner status ##
+urlScanOverview = urlArtifact + artifactReference + '?with_scan_overview=true'
+scanStatus = 'Pending'
+maxApiCall = 5
 
-    if harborDbHash != pipelineDbHash:
-        print('true')
+while scanStatus != 'Success':
+    scanOverviewResp = requests.get(urlScanOverview, auth=(username, password))
+    scanOverviewResult = scanOverviewResp.json()['scan_overview']['application/vnd.scanner.adapter.vuln.report.harbor+json; version=1.0']
+    scanStatus = scanOverviewResult['scan_status']
+    print(scanStatus)
+    if scanStatus == 'Success':
+        break
+    elif maxApiCall <= 0:
+        print('Reached maximum API calls')
+        sys.exit(-1)
     else:
-        print('false')
+        maxApiCall -= 1
+        time.sleep(4)
 
-## Build DB when image doesn't exist or something goes wrong ##
-except:
-    print('true')
+print(json.dumps(scanOverviewResult['summary'], indent=4))
+
